907b4f5687086c3816fbe305e6b97288417f6ce5
TrubitsynEgor <trubitsynwork74@gmail.com>
2023-05-20 08:38:37 +0500

    +events.js

---
+++ lessons/events.js
@@ -0,0 +1,17 @@
+const Emitter = require('events')
+
+const emitter = new Emitter()
+
+emitter.on('message', (data, second) => {
+  console.log(`Вы прислали сообщение  ${data}`)
+  console.log(`Второй аргумент  ${second}`)
+})
+
+const MESSAGE = process.env.MESSAGE || ''
+console.log(process.env.MESSAGE)
+
+if (MESSAGE) {
+  emitter.emit('message', MESSAGE, 123)
+} else {
+  emitter.emit('message', 'Вы не указали сообщение')
+}
